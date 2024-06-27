c5890a0c8eba5a8af0e8369e1022a8bdc290c5ba
Lucas Lapazini <lucaslapazini95@gmail.com>
2024-06-27 17:27:44 -0300

    feat: search bar on header component WIP
    WIP

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,30 +6,63 @@ import starIcon from "../assets/icons/star.svg";
 import searchIcon from "../assets/icons/search.svg";
 import plusIcon from "../assets/icons/plus.svg";
 import userIcon from "../assets/logged_user.svg";
+import closeIcon from "../assets/icons/close.svg";
 import { useState } from "react";
 import DropdownMenuUser from "./DropdownMenuUser";
 
 const Header = () => {
   const [openDropdownMenu, setOpenDropdownMenu] = useState(false);
+  const [showSearchBox, setShowSearchBox] = useState(false);
+
+  const toggleSearchBox = () => {
+    setShowSearchBox((prev) => !prev);
+  };
 
   return (
     <div className="relative">
       <div className="flex flex-col md:flex-row gap-4 py-4 px-8 items-center md:justify-between bg-black">
         <img src={compassLogo} alt="compassLogo" className="w-[200px]" />
         <div className="flex flex-row md:order-2 justify-center items-center font-workSans text-base text-white gap-4 w-full md:justify-end">
-          <div className="flex flex-row gap-2">
-            <button className="py-2 px-4 flex flex-row justify-center items-center gap-2">
-              <img src={searchIcon} alt="searchIcon" />
-              <p>Buscar</p>
-            </button>
-            <a
-              href="#"
-              className="py-2 px-4 flex flex-row justify-center items-center gap-2"
-            >
-              <img src={plusIcon} alt="plusIcon" />
-              <p>Minha Lista</p>
-            </a>
-          </div>
+          {showSearchBox ? (
+            <div className="bg-slate-400 px-3 py-4 flex flex-col gap-4">
+              <input type="text" placeholder="Filme, série ou celebridade" />
+              <div className="flex flex-row gap-3">
+                <button className="h-[43px] p-3 border-solid">
+                  <select name="options" id="options">
+                    <option value="Tudo">Tudo</option>
+                    <option value="Filmes">Filmes</option>
+                    <option value="Coleções">Coleções</option>
+                    <option value="Séries">Séries</option>
+                    <option value="Celebridades">Celebridades</option>
+                  </select>
+                </button>
+                <button>
+                  <img src={searchIcon} alt="searchIcon" />
+                </button>
+                <button onClick={toggleSearchBox}>
+                  <img src={closeIcon} alt="closeIcon" />
+                </button>
+              </div>
+            </div>
+          ) : (
+            <div className="flex flex-row gap-2">
+              <button
+                onClick={toggleSearchBox}
+                className="py-2 px-4 flex flex-row justify-center items-center gap-2"
+              >
+                <img src={searchIcon} alt="searchIcon" />
+                <p>Buscar</p>
+              </button>
+              <a
+                href="#"
+                className="py-2 px-4 flex flex-row justify-center items-center gap-2"
+              >
+                <img src={plusIcon} alt="plusIcon" />
+                <p>Minha Lista</p>
+              </a>
+            </div>
+          )}
+
           <button
             onClick={() => setOpenDropdownMenu((prev) => !prev)}
             aria-haspopup="true"
