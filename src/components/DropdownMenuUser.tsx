0333dce9b23c5e8eaa57d20377750e3d24292316
Vinícius Gonzato <vinigonzatofw@gmail.com>
2024-06-30 18:04:13 -0300

    Fix(DropDown): Fix alignment and styling of DropDown header

--- src/components/DropdownMenuUser.tsx
+++ src/components/DropdownMenuUser.tsx
@@ -1,5 +1,6 @@
 import addUserIcon from "../assets/icons/add_user_icon.svg";
 import dummyUser from "../assets/user_dummy.svg";
+import dummyUser2 from "../assets/user_dummy2.svg";
 import "./dropdownMenuUser.css";
 
 const DropdownMenuUser = () => {
@@ -8,17 +9,23 @@ const DropdownMenuUser = () => {
       <div className="flex flex-col gap-4">
         <button className="flex flex-row items-center gap-4">
           <img src={dummyUser} alt="dummyUser" />
-          <p>Leslie Alexander</p>
+          <p className="opacity-80">Leslie Alexander</p>
         </button>
         <button className="flex flex-row items-center gap-4">
-          <img src={addUserIcon} alt="addUserIcon" />
-          <p>Criar perfil</p>
+          <img src={dummyUser2} alt="dummyUser2" />
+          <p className="opacity-80">Ronald Richards</p>
+        </button>
+        <button className="flex flex-row items-center gap-4">
+          <div className="flex justify-center bg-neutral-500 rounded-full">
+            <img className="p-[17px]" src={addUserIcon} alt="addUserIcon" />
+          </div>
+          <p className="opacity-80">Criar perfil</p>
         </button>
       </div>
-      <ul className="flex flex-col gap-6">
+      <ul className="flex flex-col gap-6 items-start">
         <li>Editar perfis</li>
         <li>Preferências</li>
-        <li>Minha assinatura</li>
+        <li className="text-primary-100 font-bold">Minha assinatura</li>
         <li>Minha conta</li>
         <li>Ajuda</li>
         <li>Sair</li>
