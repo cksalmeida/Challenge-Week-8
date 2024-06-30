7fda7bbd643aceb34d5bcd7407fb4b0cef321100
Mariana Kemmerich <marianarkemmerich@gmail.com>
2024-06-30 01:01:54 -0300

    fix: Correct typo in HeroInformation

--- src/components/HeroInformation.tsx
+++ src/components/HeroInformation.tsx
@@ -110,7 +110,7 @@ const HeroInformation = ({ detail }: Props) => {
           ""
         )}
         <div className="flex gap-6">
-          <Tooltip text='Adicionar à "Assistir mais tarde'>
+          <Tooltip text='Adicionar à "Assistir mais tarde"'>
           <ButtonRounded img={addVector} hoverImg={addHoverVector} alt="Add" />
           </Tooltip>
           <Tooltip text='Adicionar aos Favoritos'>
