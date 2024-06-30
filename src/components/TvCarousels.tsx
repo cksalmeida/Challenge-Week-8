175eafb342266dfc53324a52e528bcc0c29e630d
Davi <davimathais@gmail.com>
2024-06-30 19:14:32 -0300

    feat: Collection Carousel

--- src/components/TvCarousels.tsx
+++ src/components/TvCarousels.tsx
@@ -29,8 +29,7 @@ const TvCarousels = () => {
         list = list.filter((item: object) => item.poster_path !== null);
         setListOnTheAir(list);
       };
-    
-      // Função para buscar séries de TV mais bem avaliadas
+  
       const fetchTopRatedTvItems = async () => {
         const list = await fetchTopRatedTVShows();
         setListTopRatedTvs(list);
@@ -58,12 +57,12 @@ const TvCarousels = () => {
       <Carrossel 
         query={listOnTheAir} 
         page="series" 
-        title="Estão no ar" 
+        title={"Estão no ar"}
       />
       <Carrossel 
         query={listTopRatedTvs} 
         page="series" 
-        title="Mais Bem Avaliadas" 
+        title={"Mais Bem Avaliadas"}
       />
     </div>
   );
