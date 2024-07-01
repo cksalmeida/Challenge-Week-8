72228e01c88fa4a2472b1f1ea2ecb3f5f9703fab
Vinícius Gonzato <vinigonzatofw@gmail.com>
2024-07-01 13:01:05 -0300

    Fix: Fix Movie page components

--- src/components/MovieSearch.tsx
+++ src/components/MovieSearch.tsx
@@ -10,6 +10,7 @@ interface MovieSearchProps {
 interface Movie {
   id: number;
   poster_path: string;
+  name: string;
 }
 
 const MovieSearch: React.FC<MovieSearchProps> = ({ query, category }) => {
@@ -19,8 +20,11 @@ const MovieSearch: React.FC<MovieSearchProps> = ({ query, category }) => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const data = await fetchAxiosSearch(category, query);
-        setMovies(data.results);
+        let data = await fetchAxiosSearch(category, query);
+        data = data.results.filter(
+          (item: { poster_path: string | null }) => item.poster_path !== null
+        );
+        setMovies(data);
         setSearchText(query);
       } catch (error) {
         console.error("Error fetching data: ", error);
@@ -33,10 +37,11 @@ const MovieSearch: React.FC<MovieSearchProps> = ({ query, category }) => {
   }, [query, category]);
 
   return (
-    <div>
+    <div className="container mx-auto h-[150vh]">
       {query && (
-        <div className="search-info">
-          Resultados para sua busca: <strong>{searchText}</strong>
+        <div className="search-info text-white">
+          <span className="opacity-80">Resultados para sua busca: </span>
+          <span className="font-bold">{searchText}</span>
         </div>
       )}
       <div className="movies-grid">
@@ -44,12 +49,14 @@ const MovieSearch: React.FC<MovieSearchProps> = ({ query, category }) => {
           <div key={movie.id} className="movie-item">
             <img
               src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-              alt=""
+              alt={movie.name}
             />
           </div>
         ))}
       </div>
-      {movies.length === 0 && <div className="no-results">Sem resultados</div>}
+      {movies.length === 0 && (
+        <div className="no-results text-white opacity-80">Sem resultados</div>
+      )}
     </div>
   );
 };
